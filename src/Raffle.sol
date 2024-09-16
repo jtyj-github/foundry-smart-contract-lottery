85e1236f8ed827a61cdf9ad32f8ecdfc4e1def05
jtyj-github <tanyjjustin@gmail.com>
2024-09-16 23:13:12 +0800

    ⚡️feat: smart contract raffle setup

---
+++ src/Raffle.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.19;
+
+/**
+    * @title Raffle
+    * @dev Justin Tan
+    * @notice This contract is for creating a simple raffle
+    * @dev Implement Chainlink VRFv2.5
+*/
+contract Raffle {
+    uint256 private immutable i_entranceFee;
+
+    constructor(uint256 entranceFee) {
+        i_entranceFee = entranceFee;
+    }
+
+    function enterRaffle() public payable{
+
+    }
+
+    function pickWinner() public {}
+
+    /** Getter Functions */
+    function getEntranceFee() external view returns(uint256) {
+        return i_entranceFee;
+    }
+}
