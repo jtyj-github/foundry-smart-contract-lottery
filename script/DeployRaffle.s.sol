cba7607c7129169a796ea4a5242be8fc6b01eb3f
jtyj-github <tanyjjustin@gmail.com>
2024-09-23 22:12:32 +0800

    🐛fix: wrong import statements

--- script/DeployRaffle.s.sol
+++ script/DeployRaffle.s.sol
@@ -4,7 +4,7 @@ pragma solidity 0.8.19;
 import { Script } from "forge-std/Script.sol";
 import { Raffle } from "src/Raffle.sol";
 import { HelperConfig } from "script/HelperConfig.s.sol";
-import {CreateSubscription} from "script/CreateSubscription.s.sol";
+import {CreateSubscription} from "script/Interactions.s.sol";
 
 contract DeployRaffle is Script {
     function run() public {}
