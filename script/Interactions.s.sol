4b3e242ca208d1ff333ff3407c457636cb4fab8a
jtyj-github <tanyjjustin@gmail.com>
2024-09-23 21:55:06 +0800

    ⚡️feat: create Interactions script

---
+++ script/Interactions.s.sol
@@ -0,0 +1,39 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.19;
+
+import {Script, console} from "forge-std/Script.sol";
+import {HelperConfig} from "script/HelperConfig.s.sol";
+import {VRFCoordinatorV2_5Mock} from "@chainlink/contracts/src/v0.8/vrf/mocks/VRFCoordinatorV2_5Mock.sol";
+
+contract CreateSubscription is Script{
+    function createSubscriptionUsingConfig() public returns (uint256, address){
+        HelperConfig helperConfig = new HelperConfig();
+        address vrfCoordinator = helperConfig.getConfig().vrfCoordinator;
+        (uint256 subId, )= createSubsciption(vrfCoordinator);
+        return (subId, vrfCoordinator);
+    }
+
+    function createSubsciption(address vrfCoordinator) public returns (uint256, address){
+        console.log("Creating subscription on chainId:", block.chainid);
+        vm.startBroadcast();
+        uint256 subId = VRFCoordinatorV2_5Mock(vrfCoordinator).createSubscription();
+        vm.stopBroadcast();
+        console.log("Subscription created with id:", subId);
+        console.log("Please update the subscriptionId in HelperConfig.s.sol");
+        return (subId, vrfCoordinator);
+    }
+
+    function run() public {}
+}
+
+contract FundSubscription is Script{
+    uint256 public constant FUND_AMOUNT = 3 ether;
+
+    function fundSubscriptionUsingConfig() public {
+        HelperConfig helperConfig = new HelperConfig();
+        address vrfCoordinator = helperConfig.getConfig().vrfCoordinator;
+        uint256 subscriptionId = helperConfig.getConfig().subscriptionId;
+    }
+
+    function run() public {}
+}
