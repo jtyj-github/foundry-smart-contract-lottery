b4a34912e0743572a4390f0e0a5f11764608755e
jtyj-github <tanyjjustin@gmail.com>
2024-09-23 22:39:00 +0800

    🐛fix: improper method called

--- script/Interactions.s.sol
+++ script/Interactions.s.sol
@@ -81,7 +81,7 @@ contract AddConsumer is Script {
     }
 
     function run() external {
-        address mostRecentlyDeployed = DevOpsTools.get_most_recently_deployed("Raffle", block.chainid);
+        address mostRecentlyDeployed = DevOpsTools.get_most_recent_deployment("Raffle", block.chainid);
         addConsumerUsingConfig(mostRecentlyDeployed);
     }
 }
